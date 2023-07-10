adcbcfa5649d5fb2044033b06e862d438411b30d
RAFIUL ISLAM <124669101+rafiulislam0006@users.noreply.github.com>
2023-07-11 00:21:20 +0600

    Add files via upload

---
+++ 4beats_RAFIUL_SQA_TASK/src/test/java/BaseDriver.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseDriver {
+    public static WebDriver driver;
+
+    @BeforeSuite
+    public void start(){
+        String browser = System.getProperty("browser", "chrome");
+
+        if(browser.contains("chrome")){
+
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        }
+        // else if(browser.contains("opera")){
+        //     WebDriverManager.operadriver().setup();
+        //     driver = new OperaDriver();
+        // }
+        else if(browser.contains("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        else{
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+    }
+
+    @AfterSuite
+    public void close(){
+        driver.quit();
+    }
+}
--- 4beats_RAFIUL_SQA_TASK/src/test/java/ReadingDataFromExcel.java
+++ 4beats_RAFIUL_SQA_TASK/src/test/java/ReadingDataFromExcel.java
@@ -1,9 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,13 +20,14 @@ import java.io.IOException;
 
 public class ReadingDataFromExcel {
 
+
     public static void main(String[] args) throws IOException {
 
         FileInputStream file = new FileInputStream("C://Users//rafi//Desktop//test.xlsx");
 
         XSSFWorkbook workbook = new XSSFWorkbook(file);
 
-        XSSFSheet sheet = workbook.getSheet("Saturday");
+        XSSFSheet sheet = workbook.getSheet("Monday");
 
         int rowcount = sheet.getLastRowNum(); // return the row count
 
@@ -32,6 +42,17 @@ public class ReadingDataFromExcel {
             }
             System.out.println();
         }
+
+    }
+
+
+    public static String baseUrl = "https://www.google.com";
+
+    @Test
+    public void openUrl() throws InterruptedException {
+        BaseDriver.driver.get(baseUrl);
+        BaseDriver.driver.manage().window().maximize();
+        Thread.sleep(1000);
 
     }
 }
